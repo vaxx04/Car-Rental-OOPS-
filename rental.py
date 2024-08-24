40c887ea5fd7468dd58233a9cc8e08ba746b1440
vaxx04 <166910087+vaxx04@users.noreply.github.com>
2024-08-24 11:34:10 +0530

    Add files via upload

---
+++ rental.py
@@ -0,0 +1,6 @@
+class Rental:
+    def __init__(self,rental_id,customer,car,rental_fee):
+        self.rental_id = rental_id
+        self.customer = customer
+        self.car = car
+        self.rental_fee = rental_fee
